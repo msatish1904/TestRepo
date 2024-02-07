abbc1e71d1cdf382a26f60fceb0da3b807454eb8
msatish1904 <99471611+msatish1904@users.noreply.github.com>
2024-02-07 17:11:27 +0530

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-    description = "Name of the s3 bucket"
-    value = {for k,v in aws_s3_bucket.s3: k=>v.id}
-}
-
-output "arn" {
-    description = "ARN of the s3 bucket"
-    value = {for k,v in aws_s3_bucket.s3: k=>v.arn}
-}
