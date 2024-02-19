2451ce5d7facb06612234abb5eafa02c597fbb2e
msatish1904 <99471611+msatish1904@users.noreply.github.com>
2024-02-20 00:12:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,17 +4,6 @@ variable "region"{
   default     = "us-east-1"
 }
 
-variable "aws_access_key" {
-  description = "Key name of the Key Pair to use for the instance; which can be managed using the `aws_key_pair` resource"
-  type        = string
-  default     = "AKIAW3MEFAISQDWECBAE5Z"
-}
-
-variable "aws_secret_key" {
-  description = "Key name of the Key Pair to use for the instance; which can be managed using the `aws_key_pair` resource"
-  type        = string
-  default     = "QL2KFGCd9kFPrezoZiR+mfummFxS8oiiViECbathwsM"
-}
 variable "s3_config" {
   description = "details of the s3 configuration"
   type = map(object({
